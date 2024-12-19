b6730d2b4c88b42a9475d9308ac29e85b756afa8
Charlotte <61674780+lotte25@users.noreply.github.com>
2024-12-19 19:31:46 -0300

    Update DynamicEnum.cpp

--- src/settings/DynamicEnum.cpp
+++ src/settings/DynamicEnum.cpp
@@ -17,7 +17,7 @@ matjson::Value matjson::Serialize<DynamicEnumValue>::toJson(const DynamicEnumVal
 }
 
 Result<DynamicEnumValue> matjson::Serialize<DynamicEnumValue>::fromJson(const matjson::Value& json) {
-    return DynamicEnumValue(json.asString());
+    return DynamicEnumValue(json.asString().unwrap());
 }
 
 std::unordered_map<std::string, std::vector<std::function<void()>>> DynamicEnum::loaders;
